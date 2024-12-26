5ab19aa8ba90964ff5296e986136ca3c2d0098cd
fei <1976649718@qq.com>
2024-12-26 17:06:49 +0800

    hot-fix commit1

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("Hello World 1");
         System.out.println("Hello World 2");
         System.out.println("Hello World 3");
+        System.out.println("Hello World 4");
     }
 }
